374ea2126ab2f305355bb0b3481022bc426d6a14
GGolfz <wisarutgolf11@gmail.com>
2021-08-06 19:27:37 +0700

    add: cli packages :sparkles:

---
+++ packages/cli/index.ts
@@ -0,0 +1,28 @@
+import { Generator,Exportor } from "@tcgen/core";
+import { Command } from "commander";
+const program = new Command();
+
+program.command("generate <template>").description("generate a testcase").action(generateTestCase)
+program.option("-n, --num [num]",'number of testcases').option("-e, --extension [extension]","file extension");
+program.parse(process.argv);
+
+interface GenerateTestCaseOptions {
+    num?: number;
+    extension?: string;
+}
+async function generateTestCase(template: string) {
+    const options = program.opts<GenerateTestCaseOptions>();
+    let extension = 'in';
+    if(options.extension) extension = options.extension;
+    let testcase
+    if(options.num) {
+        for(let i = 1 ; i=options.num; i++) {
+            testcase = Generator.generate(template);
+            await Exportor.writeFile(`${i}.${extension}`,testcase);
+        }
+    } else {
+        testcase = Generator.generate(template);
+        await Exportor.writeFile(`testcase.${extension}`,testcase);
+    }
+    console.log("Generate Event Success!")
+}
