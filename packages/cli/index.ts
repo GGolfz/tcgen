c0110f5dea652da8288252262c609acc50de9ce8
GGolfz <wisarutgolf11@gmail.com>
2021-08-06 21:12:59 +0700

    add: read from file :sparkles:

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,25 +1,31 @@
 import { Generator, Exportor } from "@tcgen/core";
 import { Command } from "commander";
+import fs from "fs";
 const program = new Command();
 
 program
-  .command("generate <template>")
+  .command("generate [template]")
   .description("generate a testcase")
   .action(generateTestCase);
 program
   .option("-n, --num [num]", "number of testcases")
-  .option("-e, --extension [extension]", "file extension");
+  .option("-e, --extension [extension]", "file extension")
+  .option("-s, --source [source]", "template source");
 program.parse(process.argv);
 
 interface GenerateTestCaseOptions {
   num?: number;
   extension?: string;
+  source?: string;
 }
-async function generateTestCase(template: string) {
+async function generateTestCase(template?: string) {
   const options = program.opts<GenerateTestCaseOptions>();
   let extension = "in";
   if (options.extension) extension = options.extension;
   let testcase;
+  if (!template) {
+    template = await fs.readFileSync("../testcase/template.txt", "utf8");
+  }
   if (options.num) {
     for (let i = 1; i <= options.num; i++) {
       testcase = Generator.generate(template);
