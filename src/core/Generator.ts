5535aee1745be22c12d1e413cc4f0e303217db42
GGolfz <wisarutgolf11@gmail.com>
2021-08-06 18:44:07 +0700

    add: Generator :sparkles:

---
+++ src/core/Generator.ts
@@ -0,0 +1,78 @@
+import {
+  Input,
+  StringInput,
+  FloatInput,
+  IntegerInput,
+  RegexInput,
+} from "../@types/Input";
+import { Extractor } from "./Extractor";
+const RandExp = require("randexp");
+
+export class Generator {
+  static generate(template: string) {
+    const inputsList: Array<Array<Input>> = Extractor.extract(template);
+    let result: string = "";
+    for (let i = 0; i < inputsList.length; i++) {
+      let line = inputsList[i] ?? [];
+      let lineResult: Array<string> = [];
+      for (let j = 0; j < line.length; j++) {
+        let val = line[j];
+        if (val) {
+          switch (val.type) {
+            case "string":
+              let stringInput: StringInput = val as StringInput;
+              lineResult.push(this.generateFromString(stringInput));
+              break;
+            case "float":
+              let floatInput: FloatInput = val as FloatInput;
+              lineResult.push(this.generateFromFloating(floatInput));
+              break;
+            case "integer":
+              let integerInput: IntegerInput = val as IntegerInput;
+              lineResult.push(this.generateFromInteger(integerInput));
+              break;
+            case "regex":
+              let regexInput: RegexInput = val as RegexInput;
+              lineResult.push(this.generateFromRegex(regexInput.regex));
+              break;
+          }
+        }
+      }
+      result += lineResult.join(" ") + "\n";
+    }
+    return result;
+  }
+
+  static generateFromString(input: StringInput): string {
+    let result: string = "";
+    result = this.generateFromRegex(
+      `[a-zA-z]{${input.minLength},${input.maxLength}}`
+    );
+    return result;
+  }
+  static generateFromInteger(input: IntegerInput): string {
+    let result: string = "";
+    let min: number = input.minValue;
+    let max: number = input.maxValue;
+    let random: number = Math.random() * (max - min) + min;
+    result = Math.round(random).toString();
+    return result;
+  }
+  static generateFromFloating(input: FloatInput): string {
+    let result: string = "";
+    let min: number = input.minValue;
+    let max: number = input.maxValue;
+    let digits: number = input.digits;
+    let random: number = Math.random() * (max - min) + min;
+    let str: string = random.toFixed(digits);
+    result = str;
+    return result;
+  }
+  static generateFromRegex(regex: string): string {
+    let result: string = "";
+    try {
+      result = new RandExp(regex).gen();
+    } catch (err) {}
+    return result;
+  }
+}
